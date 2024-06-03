7a57e7ae3ef13b1ae97717e5f23172bac6d9049c
arjaypogi12 <yellerivera14@gmail.com>
2024-06-03 09:29:51 +0800

    weather icon added

---
+++ app/script_news.js
@@ -0,0 +1,9 @@
+function showSection(sectionId) {
+    document.querySelectorAll('section').forEach(function(section) {
+        section.style.display = 'none';
+    });
+  
+    document.getElementById(sectionId).style.display = 'block';
+  }
+  
+  showSection('home'); 
